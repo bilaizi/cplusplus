d842ae0cbf18d1937f7531a898ee21fb7a86c8b6
bilaizi <2512075115@qq.com>
2018-03-18 21:53:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ using namespace std;
 
 class Request{
 public:
-    Request(int id) : mId{id}{ }
+    Request(int id) : mId{ id }{ }
     void process(){
 	cout << "Processing request " << mId << endl;
     }
@@ -84,7 +84,7 @@ private:
     int mId;
 };
 int main(){
-    Request req(100);
+    Request req{ 100 };
     thread t{ &Request::process, &req };
     t.join();
     return 0;
