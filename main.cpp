14b002fafbaff6283e5de43c9e80bac856a4f50c
bilaizi <2512075115@qq.com>
2018-03-18 21:51:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -70,6 +70,26 @@ struct year {
 
 
 /* 
+#include <thread>
+#include <iostream>
+using namespace std;
+
+class Request{
+public:
+    Request(int id) : mId{id}{ }
+    void process(){
+	cout << "Processing request " << mId << endl;
+    }
+private:
+    int mId;
+};
+int main(){
+    Request req(100);
+    thread t{ &Request::process, &req };
+    t.join();
+    return 0;
+}
+
 #include <exception>
 #include <iostream>
 #include <stdexcept>
