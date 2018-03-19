ae73eb0e0f96ebb4e3dc3d9873137efa0ac854c0
bilaizi <2512075115@qq.com>
2018-03-19 11:23:04 +0800

    Update concurrency.cpp

--- concurrency.cpp
+++ concurrency.cpp
@@ -91,20 +91,20 @@ class function_wrapper{
     template<typename F>
     struct impl_type: impl_base{
         F f;
-        impl_type(F&& f_): f(std::move(f_)) {}
+        impl_type(F&& f_): f{ std::move(f_) }{}
         void call() { f(); }
     };
 public:
     template<typename F>
-    function_wrapper(F&& f):impl(new impl_type<F>(std::move(f))){}
+    function_wrapper(F&& f):impl{ new impl_type<F>{std::move(f)} }{}
     void call() { impl->call(); }
-    function_wrapper(function_wrapper&& other):impl(std::move(other.impl)){}
+    function_wrapper(function_wrapper&& other):impl{ std::move(other.impl }{}
     function_wrapper& operator=(function_wrapper&& other){
         impl=std::move(other.impl);
         return *this;
     }
-    function_wrapper(const function_wrapper&)=delete;
     function_wrapper(function_wrapper&)=delete;
+    function_wrapper(const function_wrapper&)=delete;
     function_wrapper& operator=(const function_wrapper&)=delete;
 };
 
