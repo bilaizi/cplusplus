982a447325cca9bd730cc985dcabeb0ff24bb83d
bilaizi <2512075115@qq.com>
2018-03-19 16:10:22 +0800

    Update concurrency.cpp

--- concurrency.cpp
+++ concurrency.cpp
@@ -39,6 +39,43 @@ function object in new thread is created by: move constructor
 /*
 
 // Chapter8
+// listing_8.10.cpp
+template<typename Iterator,typename MatchType>
+Iterator parallel_find_impl(Iterator first, Iterator last, MatchType match, std::atomic<bool>& done){
+    try{
+        const unsigned long length=std::distance(first,last);
+        const unsigned long min_per_thread=25;
+        if(length<(2*min_per_thread)){
+            for(; (first!=last) && !done.load();++first){
+                if(*first==match){
+                    done=true;
+                    return first;
+                }
+            }
+            return last;
+        }else{
+            Iterator const mid_point=first+(length/2);
+            std::future<Iterator> async_result=std::async(
+	        &parallel_find_impl<Iterator, MatchType>,
+                mid_point,
+		last,
+		match,
+		std::ref(done)
+            );
+            Iterator const direct_result = parallel_find_impl(first,mid_point,match,done);
+            return direct_result==mid_point ? async_result.get() : direct_result;
+        }
+    }catch(...){
+        done=true;
+        throw;
+    }
+}
+template<typename Iterator,typename MatchType>
+Iterator parallel_find(Iterator first, Iterator last, MatchType match){
+    std::atomic<bool> done{ false };
+    return parallel_find_impl(first, last, match, done);
+}
+
 // listing_8.11.cpp
 template<typename Iterator>
 void parallel_partial_sum(Iterator first,Iterator last){
