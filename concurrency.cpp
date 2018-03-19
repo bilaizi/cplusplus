c29bb542b73d1ae5297fad5c878be6aa58da6f7a
bilaizi <2512075115@qq.com>
2018-03-19 10:39:54 +0800

    Create concurrency.cpp

---
+++ concurrency.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+class C {
+    std::string s;
+public:
+    void operator() () const {
+        std::cout << "function object in new thread is created by: " + s << std::endl;
+    }
+    C() = default;
+    C(const C& another) {
+        s = "copy constructor";
+        std::cout << s << std::endl;
+    }
+    C(C&& another) {
+        s = "move constructor";
+        std::cout << s << std::endl;
+    }
+};
+
+auto main()->int {
+    C c;
+    std::thread t{c};
+    t.join();
+    std::thread t2{C{}};
+    t2.join();
+    return 0;
+}
