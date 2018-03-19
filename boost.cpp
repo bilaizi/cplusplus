4441dfa2d653c1da41b21a38c98637e465f2f4f2
bilaizi <2512075115@qq.com>
2018-03-19 20:11:41 +0800

    Update boost.cpp

--- boost.cpp
+++ boost.cpp
@@ -16,3 +16,103 @@ int main(int argc, char* argv[]){
     t.wait();
     return 0;
 }
+/*
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <boost/asio/ts/buffer.hpp>	
+#include <boost/asio/ts/executor.hpp>
+#include <boost/asio/ts/internet.hpp>
+#include <boost/asio/ts/io_context.hpp>
+#include <boost/asio/ts/net.hpp>
+#include <boost/asio/ts/netfwd.hpp>
+#include <boost/asio/ts/timer.hpp>
+#include <boost/asio/ts/socket.hpp>
+
+using namespace std::chrono_literals;
+using namespace boost::asio;
+using namespace boost::asio::ip;
+//using boost::asio::io_context;
+//using boost::asio::steady_timer;
+//using boost::asio::ip::tcp;
+
+//
+// reference_counted.cpp
+// 
+
+
+// A reference-counted non-modifiable buffer class.
+class shared_const_buffer{
+public:
+  // Construct from a std::string.
+  explicit shared_const_buffer(const std::string& data)
+    : data_(new std::vector<char>(data.begin(), data.end())),
+      buffer_(buffer(*data_))
+  {
+  }
+
+  // Implement the ConstBufferSequence requirements.
+  typedef const_buffer value_type;
+  typedef const const_buffer* const_iterator;
+  const const_buffer* begin() const { return &buffer_; }
+  const const_buffer* end() const { return &buffer_ + 1; }
+
+private:
+  std::shared_ptr<std::vector<char>> data_;
+  const_buffer buffer_;
+};
+
+class session : public std::enable_shared_from_this<session>{
+public:
+    session(tcp::socket socket) : socket_{ std::move(socket) }{}
+    void start(){
+        do_write();
+    }
+private:
+    void do_write(){
+        auto now{ std::time(0) };
+        shared_const_buffer buffer{ std::ctime(&now) };
+        auto self{ shared_from_this()};
+        boost::asio::async_write(socket_, buffer, [this, self](boost::system::error_code /*ec*/, std::size_t /*length*/){});
+    }
+    // The socket used to communicate with the client.
+    tcp::socket socket_;
+};
+
+class server{
+public:
+  server(io_context& io_context, short port): acceptor_(io_context, tcp::endpoint(tcp::v4(), port)){
+    do_accept();
+  }
+
+private:
+  void do_accept(){
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, tcp::socket socket){
+          if (!ec)
+            std::make_shared<session>(std::move(socket))->start();
+          do_accept();
+        }
+    );
+  }
+  tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[]){
+    try{
+        if(argc != 2){
+            std::cerr << "Usage: reference_counted <port>\n";
+            return 1;
+        }
+        io_context io_context;
+        server s{ io_context, atoi(argv[1]) };
+        io_context.run();
+    }catch (std::exception& e){
+        std::cerr << "Exception: " << e.what() << "\n";
+    } 
+    return 0;
+}
+
+*/
