5991d991d80ba81718ada24724153881251cc820
bilaizi <2512075115@qq.com>
2018-03-19 11:26:38 +0800

    Update concurrency.cpp

--- concurrency.cpp
+++ concurrency.cpp
@@ -112,8 +112,7 @@ class thread_pool{
 public:
     std::deque<function_wrapper> work_queue;
     template<typename FunctionType>
-    std::future<typename std::result_of<FunctionType()>::type>
-    submit(FunctionType f){
+    std::future<typename std::result_of<FunctionType()>::type> submit(FunctionType f){
         typedef typename std::result_of<FunctionType()>::type result_type;
         std::packaged_task<result_type()> task(std::move(f));
         std::future<result_type> res(task.get_future());
