afca5922627f0695ca201884d353535fdc3d9d8c
bilaizi <2512075115@qq.com>
2018-03-21 03:02:41 +0800

    Update boost.cpp

--- boost.cpp
+++ boost.cpp
@@ -17,6 +17,78 @@ int main(int argc, char* argv[]){
     return 0;
 }
 /*
+// sender.cpp
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <boost/asio/ts/internet.hpp>
+#include <boost/asio/ts/io_context.hpp>
+#include <boost/asio/ts/timer.hpp>
+using namespace boost::asio;
+using namespace boost::asio::ip;
+//using boost::asio::ip::udp;
+using namespace std::chrono_literals;
+
+constexpr short multicast_port = 30001;
+constexpr int max_message_count = 10;
+
+class sender{
+public:
+    sender(io_context& io_context, ip::address& multicast_address) 
+        : endpoint_{ 
+            multicast_address, multicast_port 
+        }, socket_{ 
+            io_context, endpoint_.protocol() 
+        }, timer_{ io_context },message_count_{ 0 }{
+            do_send();
+        }
+private:
+    void do_send() {
+        std::ostringstream os;
+        os << "Message " << message_count_++;
+        message_ = os.str();
+        socket_.async_send_to(
+            buffer(message_), 
+            endpoint_, 
+            [this](boost::system::error_code ec, std::size_t length) {
+                if(!ec && message_count_ < max_message_count)
+                    do_timeout();
+            }
+        );
+    }
+    void do_timeout(){
+        timer_.expires_after(1s);
+        timer_.async_wait([this](boost::system::error_code ec) { if (!ec) do_send(); });
+    }
+
+private:
+    ip::udp::endpoint endpoint_;
+    ip::udp::socket socket_;
+    steady_timer timer_;
+    int message_count_;
+    std::string message_;
+};
+
+auto main(int argc, char* argv[])->int{
+    try {
+        if (argc != 2) {
+            std::cerr << "Usage: sender <multicast_address>\n";
+            std::cerr << "  For IPv4, try:\n";
+            std::cerr << "    sender 239.255.0.1\n";
+            std::cerr << "  For IPv6, try:\n";
+            std::cerr << "    sender ff31::8000:1234\n";
+            return 1;
+        }
+        io_context io_context;
+        sender s{ io_context, ip::make_address(argv[1]) };
+        // sender s(io_context,  ip::make_address(argv[1]));
+        io_context.run();
+    }catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+    return 0;
+}
+
 // daytime_client.cpp
 #include <array>
 #include <future>
