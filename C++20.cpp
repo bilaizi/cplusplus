532d71ead8316aae61a190e12d60e2ecf2dd5ae9
bilaizi <2512075115@qq.com>
2018-03-22 00:51:43 +0800

    Create C++20.cpp

---
+++ C++20.cpp
@@ -0,0 +1,114 @@
+#include <memory>
+
+template<class T>
+class pointer {
+public:
+    using element_type = typename std::pointer_traits<T>::element_type;
+    pointer(T value): value_{ value } { }
+    T operator->() const noexcept {
+        return value_;
+    }
+private:
+    T value_;
+};
+
+template<class T>
+class special {
+public:
+    special(T* value) : value_(value) { }
+    T* get() const noexcept {
+        return value_;
+    }
+private:
+    T* value_;
+};
+namespace std {
+    template<class T>
+    struct pointer_traits<special<T> > {
+        using pointer = special<T>;
+        using element_type = T;
+        using difference_type = std::ptrdiff_t;
+        template<class U>
+        struct rebind_to {
+            typedef special<U> type;
+        };
+        template<class U>
+        using rebind = typename rebind_to<U>::type;
+        template<class U>
+        static pointer pointer_to(U& v) noexcept {
+            return pointer(&v);
+        }
+        static element_type* to_address(const pointer& v) noexcept {
+            return v.get();
+        }
+    };
+} 
+int main(){
+    int i = 0;
+    {
+        typedef int* type;
+        type p = &i;
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<int*> type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<pointer<int*> > type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef void* type;
+        type p = &i;
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<void*> type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef const int* type;
+        type p = &i;
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<const int*> type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef special<int> type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef special<void> type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef special<const int> type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<special<int> > type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<special<void> > type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    {
+        typedef pointer<special<const int> > type;
+        type p(&i);
+        assert(std::to_address(p) == &i);
+    }
+    return 0;
+}
