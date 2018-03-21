ef347493c9c180d9c8045a8c4128f3aa24e775b2
bilaizi <2512075115@qq.com>
2018-03-21 11:59:26 +0800

    Update coroutine.cpp

--- coroutine.cpp
+++ coroutine.cpp
@@ -58,11 +58,17 @@ struct std::experimental::coroutine_traits<std::future<void>, Args...> {
   struct promise_type { 
            std::promise<void> p;
            void unhandled_exception() {}
-           auto get_return_object() { return p.get_future(); } 
+           auto get_return_object() { 
+               return p.get_future(); 
+           } 
            std::experimental::suspend_never initial_suspend() { return {}; } 
            std::experimental::suspend_never final_suspend() { return {}; } 
-           void set_exception(std::exception_ptr e) { p.set_exception(std::move(e)); } 
-           void return_void() { p.set_value(); } 
+           void set_exception(std::exception_ptr e) { 
+               p.set_exception(std::move(e)); 
+           } 
+           void return_void() { 
+               p.set_value();
+           } 
   }; 
 }; 
 
@@ -71,11 +77,16 @@ struct std::experimental::coroutine_traits<std::future<R>, Args...> {
   struct promise_type { 
     std::promise<R> p; 
     void unhandled_exception() {}
-    auto get_return_object() { return p.get_future(); } 
+    auto get_return_object() { 
+        return p.get_future(); 
+    } 
     std::experimental::suspend_never initial_suspend() { return {}; } 
     std::experimental::suspend_never final_suspend() { return {}; } 
-    void set_exception(std::exception_ptr e) { p.set_exception(std::move(e)); } 
-    template <typename U> void return_value(U &&u) { 
+    void set_exception(std::exception_ptr e) { 
+        p.set_exception(std::move(e)); 
+    } 
+    template <typename U> 
+    void return_value(U &&u) { 
       p.set_value(std::forward<U>(u)); 
     } 
   }; 
