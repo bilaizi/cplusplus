d31b7cee9b74a1242b7ddcb6c46831b524c2f44f
bilaizi <2512075115@qq.com>
2018-03-23 03:23:36 +0800

    Update boost.cpp

--- boost.cpp
+++ boost.cpp
@@ -1342,4 +1342,43 @@ int main(int argc, char* argv[]){
     }
     return 0;
 }
+
+// blocking_tcp_echo_client.cpp
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
+#include <boost/asio/ts/buffer.hpp>	
+#include <boost/asio/ts/io_context.hpp>
+#include <boost/asio/ts/net.hpp>
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+enum { max_length = 1024 };
+int main(int argc, char* argv[]){
+    try {
+        if (argc != 3) {
+            std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+            return 1;
+        }
+        io_context io_context;
+        tcp::socket s{ io_context };
+        tcp::resolver resolver{ io_context };
+        connect(s, resolver.resolve(argv[1], argv[2]));
+        std::cout << "Enter message: ";
+        char request[max_length];
+        std::cin.getline(request, max_length);
+        size_t request_length = std::strlen(request);
+        write(s, boost::asio::buffer(request, request_length));
+        char reply[max_length];
+        size_t reply_length = read(s, mutable_buffer{ reply, request_length });
+        std::cout << "Reply is: ";
+        std::cout.write(reply, reply_length);
+        std::cout << "\n";
+    } catch (std::exception& e){
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+    return 0;
+}
 */
