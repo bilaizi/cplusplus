66818bb72a45e94017d240fc0a0fa1da66f29dbb
bilaizi <2512075115@qq.com>
2018-03-23 02:43:24 +0800

    Update boost.cpp

--- boost.cpp
+++ boost.cpp
@@ -1142,6 +1142,7 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
+// async_tcp_echo_server.cpp
 #include <cstdlib>
 #include <chrono>
 #include <iostream>
@@ -1233,4 +1234,64 @@ int main(int argc, char* argv[]){
     }
     return 0;
 }
+
+// async_udp_echo_server.cpp
+#include <cstdlib>
+#include <iostream>
+#include <string_view>
+#include <boost/asio/ts/buffer.hpp>	
+#include <boost/asio/ts/io_context.hpp>
+#include <boost/asio/ts/net.hpp>
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+class server{
+public:
+    server(io_context& io_context, unsigned short port)
+        : socket_{io_context, udp::endpoint{ udp::v4(), port }}{
+            do_receive();
+    }
+    void do_receive() {
+        socket_.async_receive_from(
+            mutable_buffer{ data_, max_length },
+            sender_endpoint_,
+            [this](boost::system::error_code ec, std::size_t bytes_recvd) {
+                if (!ec && bytes_recvd > 0)
+                    do_send(bytes_recvd);
+                else
+                    do_receive();
+            }
+        );
+    }
+    void do_send(std::size_t length){
+        socket_.async_send_to(
+            const_buffer{ data_, max_length }, 
+            sender_endpoint_,
+            [this](boost::system::error_code ec, std::size_t bytes_sent) {
+                do_receive();
+            }
+        );
+    }
+private:
+    udp::socket socket_;
+    udp::endpoint sender_endpoint_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+int main(int argc, char* argv[]){
+    try {
+        if (argc != 2) {
+            std::cerr << "Usage: async_udp_echo_server <port>\n";
+        }
+        io_context io_context;
+        server s{ io_context, std::atoi(argv[1]) };
+        io_context.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+    return 0;
+}
+
 */
