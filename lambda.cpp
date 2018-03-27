b0127dbef5ee153fe03216c503cb0da3cdc67bbf
bilaizi <2512075115@qq.com>
2018-03-28 02:47:23 +0800

    Update lambda.cpp

--- lambda.cpp
+++ lambda.cpp
@@ -11,6 +11,25 @@ auto main() -> int {
     cout << "Original Data:" << endl;
     for_each(begin(vect), end(vect), [](int n){ cout << n << " "; });
     cout << endl;
+    for_each(
+             begin(vect),
+             end(vect),
+             [](auto n) {
+                cout << n << " is";
+                if(n < 2) {
+                    cout << " not";
+                } else {
+                    for (int j = 2; j < n; ++j) {
+                        if (n % j == 0){
+                            cout << " not";
+                            break;
+                        }
+                    }
+                }
+                cout << " prime number" << endl;
+             }
+    );
+    cout << endl;
     for_each(begin(vect), end(vect), [](int& x) { x *= 2; });
     cout << "Squared Data:" << endl;
     for_each(begin(vect), end(vect), [](int n) { cout << n << " "; });
