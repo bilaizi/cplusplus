9708c411127ebdc3bbc12e25f3f71c2d646b65b7
bilaizi <2512075115@qq.com>
2018-04-05 16:48:46 +0800

    Update templates.cpp

--- templates.cpp
+++ templates.cpp
@@ -1,3 +1,27 @@
+// Class template specialization
+#include <iostream>
+ 
+template<class T>
+class myBox {
+public:
+    T a;
+    void print() { std::cout << a << std::endl; }
+};
+
+template<>
+class myBox<bool> {
+public:
+    bool a;
+    void print() { std::cout << (a ? "true" : "false") << std::endl; }
+};
+
+auto main() -> int {
+    myBox<bool> box = { true };
+    box.print(); // true
+    myBox<int> b = { 20 };
+    b.print(); // 20
+}
+
 // Function template specialization
 #include <iostream>
  
