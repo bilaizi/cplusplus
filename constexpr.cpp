13918b48a72561a6af755067789423ff0abbef88
bilaizi <2512075115@qq.com>
2018-04-07 10:04:09 +0800

    Update constexpr.cpp

--- constexpr.cpp
+++ constexpr.cpp
@@ -17,3 +17,23 @@ auto main() -> int{
 	volatile int k = 8;
 	std::cout << factorial(k) << '\n'; // runtime
 }
+
+enum class side { BID, ASK };
+class order_book {
+	template<side S>
+	class book_side {
+		using compare = std::conditional_t<S == side::BID, std::greater<>, std::less<>>;
+		std::vector<price> levels_;
+	public:
+		void insert(order o) {
+			const auto it = lower_bound(begin(levels_), end(levels_), o.price, compare{});
+			if(it != end(levels_) && *it != o.price)
+				levels_.insert(it, o.price);
+		}
+		bool match(price p) const {
+			return compare{}(levels_.back(), p);
+		};
+	}; 
+	book_side<side::BID> bids_;
+	book_side<side::ASK> asks_;
+};
