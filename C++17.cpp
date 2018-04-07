a1b94bf84482a739486bda28d2e2ee01122bf5c6
bilaizi <2512075115@qq.com>
2018-04-07 15:51:43 +0800

    Update C++17.cpp

--- C++17.cpp
+++ C++17.cpp
@@ -1,10 +1,14 @@
 #include <array>
 #include <charconv>
 #include <iostream>
+#include <system_error>
+#include <string_view>
  
 auto main() -> int {
-    std::array<char, 10> str{"42"};
+    std::array<char, 10> str{ "42" };
     int result;
     std::from_chars(str.data(), str.data()+str.size(), result);
     std::cout << result;
+    if(auto [ptr, ec] = std::to_chars(str.data(), str.data() + str.size(), result); ec == std::errc())
+        std::cout << std::string_view(str.data(), ptr - str.data());
 }
