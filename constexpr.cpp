2c277945950577b11b96cd361869feac84c565ec
bilaizi <2512075115@qq.com>
2018-04-07 09:41:51 +0800

    Update constexpr.cpp

--- constexpr.cpp
+++ constexpr.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
+
+// C++11
 constexpr int factorial(int n) {
 	return n <= 1 ? 1 : (n * factorial(n - 1));
 }
+// c++14
 constexpr int factorial(int n) {
-int result = n;
-while(n > 1)
-result *= --n;
-return result;
+	int result = n;
+	while(n > 1)
+		result *= --n;
+	return result;
+}
+
+auto main() -> int{
+	static_assert(factorial(4) == 24); // compile-time
+	volatile int k = 8;
+	std::cout << factorial(k) << '\n'; // runtime
 }
