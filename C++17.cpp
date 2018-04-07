9051bcc278c83e4b9dabfee56e569c50ed9e9872
bilaizi <2512075115@qq.com>
2018-04-07 15:52:47 +0800

    Update C++17.cpp

--- C++17.cpp
+++ C++17.cpp
@@ -9,6 +9,6 @@ auto main() -> int {
     int result;
     std::from_chars(str.data(), str.data()+str.size(), result);
     std::cout << result;
-    if(auto [ptr, ec] = std::to_chars(str.data(), str.data() + str.size(), result); ec == std::errc())
+    if(auto [ptr, ec] = std::to_chars(str.data(), str.data() + str.size(), result); ec == std::errc{})
         std::cout << std::string_view(str.data(), ptr - str.data());
 }
