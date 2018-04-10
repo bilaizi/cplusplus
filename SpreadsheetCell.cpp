76aa093869df622753c50957500c8b2ed2958429
bilaizi <2512075115@qq.com>
2018-04-10 11:52:48 +0800

    Create SpreadsheetCell.cpp

---
+++ SpreadsheetCell.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class SpreadsheetCell{
+public:
+void setValue(double inValue);
+double getValue() const;
+void setString(std::string_view inString);
+std::string getString() const;
+private:
+std::string doubleToString(double inValue) const;
+double stringToDouble(std::string_view inString) const;
+double mValue;
+};
