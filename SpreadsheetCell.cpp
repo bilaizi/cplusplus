49ad65eb7de9c1d3c7e5787f817a7ae908dd2f5d
bilaizi <2512075115@qq.com>
2018-04-10 17:11:09 +0800

    Update SpreadsheetCell.cpp

--- SpreadsheetCell.cpp
+++ SpreadsheetCell.cpp
@@ -3,6 +3,7 @@ public:
 	SpreadsheetCell() = default;
 	SpreadsheetCell(double value);
 	SpreadsheetCell(const SpreadsheetCell& other);
+	SpreadsheetCell& operator=(const SpreadsheetCell& other);
 	void setValue(double value);
 	double getValue() const;
 private:
@@ -11,7 +12,14 @@ private:
 SpreadsheetCell::SpreadsheetCell(double value):value{ value } {}
 // SpreadsheetCell::SpreadsheetCell(const SpreadsheetCell& other) = default;
 SpreadsheetCell::SpreadsheetCell(const SpreadsheetCell& other) : value{ other.value } { }
-void SpreadsheetCell::setValue(double value) {
+SpreadsheetCell& SpreadsheetCell::operator=(constSpreadsheetCell& other) {
+	if (this == &other) {
+		return *this;
+	}
+	this->value = other.mValue;
+	return *this;
+}
+void SpreadsheetCell::setValue(d ouble value) {
 	this->value = value;
 }
 double SpreadsheetCell::getValue() const {
