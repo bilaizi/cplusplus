f04028352f984ce479a01e4d56e9d272e7a0ab9f
bilaizi <2512075115@qq.com>
2018-04-10 11:56:46 +0800

    Update SpreadsheetCell.cpp

--- SpreadsheetCell.cpp
+++ SpreadsheetCell.cpp
@@ -5,3 +5,10 @@ public:
 private:
 	double value;
 };
+
+void SpreadsheetCell::setValue(double value) {
+	this->value = value;
+}
+double SpreadsheetCell::getValue() const {
+	return this->value;
+}
