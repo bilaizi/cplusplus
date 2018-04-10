ace9d3c233da4a7c828f63bf417776bf2797d2c5
bilaizi <2512075115@qq.com>
2018-04-10 12:24:33 +0800

    Update SpreadsheetCell.cpp

--- SpreadsheetCell.cpp
+++ SpreadsheetCell.cpp
@@ -1,11 +1,12 @@
 class SpreadsheetCell {
 public:
+	SpreadsheetCell(double value);
 	void setValue(double value);
 	double getValue() const;
 private:
 	double value;
 };
-
+SpreadsheetCell::SpreadsheetCell(double value):value{value} {}
 void SpreadsheetCell::setValue(double value) {
 	this->value = value;
 }
